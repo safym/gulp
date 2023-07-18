550e7be4c03dde9fe66583852ab87552bf5885a0
safym <osafym@gmail.com>
2023-07-18 19:30:17 +0300

    add modal dialog form

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,5 +1,25 @@
-const test: string = 'test'
+const MODAL_SELECTOR = "#modal";
 
-console.log('console.log from main.ts')
+const handleClick = (event: MouseEvent) => {
+  const dialog = document.querySelector(MODAL_SELECTOR) as HTMLDialogElement;
+  const form = dialog.querySelector("form") as HTMLFormElement;
 
-console.log(document.querySelector('h1'))
+  const isBuyButton = (event.target as HTMLElement).dataset?.buybutton;
+
+  if (isBuyButton && !dialog.open) {
+    dialog.showModal();
+  } else {
+    const target = event.target as HTMLElement;
+
+    if (target.id === "modalConfirm") {
+      form.submit();
+      dialog.close();
+      alert("Confirmed!");
+    } else if (target.id === "modalCancel") {
+      form.reset();
+      dialog.close();
+    }
+  }
+};
+
+window.document.addEventListener("click", handleClick);
