55ff5a3faefd0b2f821b8273d1857a017294ff50
safym <osafym@gmail.com>
2023-07-18 19:30:26 +0300

    upd dist

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,3 +1,23 @@
-var test = 'test';
-console.log('console.log from main.ts');
-console.log(document.querySelector('h1'));
+var MODAL_SELECTOR = "#modal";
+var handleClick = function (event) {
+    var _a;
+    var dialog = document.querySelector(MODAL_SELECTOR);
+    var form = dialog.querySelector("form");
+    var isBuyButton = (_a = event.target.dataset) === null || _a === void 0 ? void 0 : _a.buybutton;
+    if (isBuyButton && !dialog.open) {
+        dialog.showModal();
+    }
+    else {
+        var target = event.target;
+        if (target.id === "modalConfirm") {
+            form.submit();
+            dialog.close();
+            alert("Confirmed!");
+        }
+        else if (target.id === "modalCancel") {
+            form.reset();
+            dialog.close();
+        }
+    }
+};
+window.document.addEventListener("click", handleClick);
